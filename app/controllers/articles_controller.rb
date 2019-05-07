35609581ff9efe35e377c6d80e6dc523f6010477
Devanshu Row <devanshu.row1993@gmail.com>
2019-05-06 21:45:15 -0400

    Complete authentication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,38 +1,30 @@
 class ArticlesController < ApplicationController
   
   before_action :set_article, only: [:edit, :update, :show, :destroy]
-  
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+
+  def index
+    @articles = Article.paginate(page: params[:page], per_page: 5)
+  end
+
   def new
    @article = Article.new
   end
 
-  def destroy
-
-    @article.destroy
-    flash[:danger] = "Article was successfully deleted"
-    redirect_to articles_path
+  def edit
+ 
   end
 
   def create
     @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
     else 
       render 'new'
     end
-  end
-
-  def show
-
-  end
-
-  def index
-    @articles = Article.paginate(page: params[:page], per_page: 5)
-  end
-
-  def edit
- 
   end
 
   def update
@@ -45,6 +37,16 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def show
+
+  end
+  
+  def destroy
+    @article.destroy
+    flash[:danger] = "Article was successfully deleted"
+    redirect_to articles_path
+  end
+
   private
 
     def set_article
@@ -53,5 +55,12 @@ class ArticlesController < ApplicationController
 
     def article_params
       params.require(:article).permit(:title, :description)
+    end
+
+    def require_same_user
+      if current_user != @article.user
+        flash[:danger] = "You can only edit/delete your own articles"
+        redirect_to root_path
+      end
     end
 end
