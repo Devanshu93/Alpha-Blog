bed824cd22ac67f9da25586bed2b5747dba00131
Devanshu Row <devanshu.row1993@gmail.com>
2019-05-06 21:55:37 -0400

    Add sign in on sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to Alpha Blog, #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
@@ -52,5 +53,4 @@ class UsersController < ApplicationController
       redirect_to root_path
     end
   end
-
 end
