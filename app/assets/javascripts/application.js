6b36e9fd184de156f729c588016286974434baa7
Devanshu Row <devanshu.row1993@gmail.com>
2019-05-19 18:26:54 -0400

    modified application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery_ujs
-//= require jquery
-//= require bootstrap-sprockets
+
 //= require rails-ujs
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
 //= require activestorage
 //= require turbolinks
 //= require_tree .
